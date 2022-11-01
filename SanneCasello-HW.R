2d03d4fd88f2d3ae32ff0c2c3bee0f07a3196dbb
scasello <117229626+scasello@users.noreply.github.com>
2022-11-01 18:56:02 -0400

    Add files via upload

---
+++ SanneCasello-HW.R
@@ -0,0 +1,23 @@
+# Let me know what you learned and what you are still confused about 
+# re: Git/GitHub.
+
+# I learned how to access repositories on github, download R and R studio
+# and the fundamentals to use this as an analysis method for my research.
+
+# Having previously coded in Matlab, I still am a little confused about and
+# often make mistakes regarding Matlab versus R syntax etc. 
+
+# In this file, also describe one of the ways that Wilson et. al. suggest is 
+# a good practice for computational reproducibility and how a tool like R 
+# and/or GitHub can be used.
+
+# Wilson et.al.discusses best software practices in Box #1 2. Software. 
+# Specifically, he says to test libraries before relying on them (something
+# I will be careful about as I am not a proficient coder) and to not comment 
+# and uncomment sections of code to control a program's behavior. 
+
+# Save the file, add it to the repository, commit the changes, 
+# and push the homework to the GitHub repository for the homework. 
+# Note: At the end of this assignment, you should have one pull
+# request and two files in your repository when finished 
+# (the edited README.md file and the file that you added). 
